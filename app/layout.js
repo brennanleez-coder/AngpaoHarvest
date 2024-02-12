59eb5af1d9ba94ed1749aad128968c99e7a63549
brennanleez-coder <brennanlee95@gmail.com>
2024-02-12 16:04:18 +0800

    add analytics

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,6 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { Analytics } from '@vercel/analytics/react';
 
 
 const inter = Inter({ subsets: ["latin"] });
@@ -12,7 +13,10 @@ export default function RootLayout({ children }) {
         <title>Angpao Harvest</title>
         <link rel="icon" href="/AngpaoHarvest.png" />
       </head>
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        {children}
+        <Analytics />
+        </body>
     </html>
   );
 }
